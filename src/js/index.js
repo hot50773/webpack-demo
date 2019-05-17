240552fb2af9ed951a567bea1eb40b6cea4571aa
Ｍindy <hot50773@gmail.com>
2019-05-17 15:10:31 +0800

    Update files

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,1 @@
-import fullname from './fullname'
-// import './style.css'
-
-const person = fullname('Json', 'Wang')
-
-// console.log(person)
-var x = document.createElement('div')
-x.innerHTML = person
-x.className = 'nameTag'
-
-document.body.appendChild(x)
+console.log('test script')
